443a052efc5ae0e2e40182ee3c89f12031b1f677
Juan Carlos Ruiz Rico <bocabyte17@hotmail.com>
2012-11-08 00:22:01 +0100

    Reto 35 Ok!

--- ProjectEuler/reto35.py
+++ ProjectEuler/reto35.py
@@ -22,16 +22,12 @@ def isprime(n):
     @return: bool
     '''
     n = abs(int(n))
-
     if n < 2:
         return False
-
     if n == 2: 
         return True    
-
     if not n & 1: 
         return False
-
     for x in range(3, int(n**0.5)+1, 2):
         if n%x == 0:
             return False
@@ -44,7 +40,7 @@ def esPrimoCircular(elem, candidatos):
     res = True
     res = res and isprime(elem)
     aux = str(elem)
-    for i in range(len(aux)):
+    for _ in range(len(aux)):
         aux = rotate(int(aux))
         res = res and isprime(rotate(aux)) and (rotate(aux) in candidatos)
     return res
